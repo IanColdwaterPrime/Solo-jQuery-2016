73ecf1fe4c37cb45365019d3c1d61df9f2e7c034
Ian <shiny@what.local>
2016-09-08 13:05:58 -0500

    fixed it

--- scripts/main.js
+++ scripts/main.js
@@ -7,28 +7,28 @@ var numBlue = 0;
 $(document).ready(function() {
     console.log( "ready!" );
 
-  $(".color-button,#red").on( "click", function () {
+  $(".red-button").on( "click", function () {
     numRed++;
     console.log(numRed);
     $("#red-total").text(numRed);
     $('<span class="color-cube red"></span>').appendTo("body");
   });
 
-  $(".color-button,#yellow").on( "click", function () {
+  $(".yellow-button").on( "click", function () {
     numYellow++;
     console.log(numYellow);
     $("#yellow-total").text(numYellow);
     $('<span class="color-cube yellow"></span>').appendTo("body");
   });
 
-  $(".color-button,#green").on( "click", function () {
+  $(".green-button").on( "click", function () {
      numGreen++;
      console.log(numGreen);
      $("#green-total").text(numGreen);
      $('<span class="color-cube green"></span>').appendTo("body");
   });
 
-  $(".color-button,#blue").on( "click", function () {
+  $(".blue-button").on( "click", function () {
      numBlue++;
      console.log(numBlue);
      $("#blue-total").text(numBlue);
